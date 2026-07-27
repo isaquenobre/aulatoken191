77c0731fa3370461396183839796b67d691b8d03
isaque <isaquenobrec@gmail.com>
2026-07-27 13:22:44 -0300

    vai

--- va189_90_91_92-springsecurity_jsonwebtokenjwt/dio-spring-security-jwt/src/main/java/dio/dio_spring_security_jwt/repository/UserRepository.java
+++ va189_90_91_92-springsecurity_jsonwebtokenjwt/dio-spring-security-jwt/src/main/java/dio/dio_spring_security_jwt/repository/UserRepository.java
@@ -6,7 +6,15 @@ import org.springframework.data.repository.query.Param;
 
 import dio.dio_spring_security_jwt.model.User;
 
-public interface UserRepository extends JpaRepository<User, Integer> {
+public interface UserRepository extends JpaRepository<User, Integer> {// JpaRepository é uma interface do Spring Data
+                                                                      // JPA que fornece métodos para realizar operações
+                                                                      // de banco de dados em entidades JPA. Ela é
+                                                                      // parametrizada com a classe da entidade (User) e
+                                                                      // o tipo do identificador da entidade (Integer).
+                                                                      // Isso significa que o UserRepository terá acesso
+                                                                      // a métodos como save, findById, findAll,
+                                                                      // deleteById, entre outros, para manipular
+                                                                      // objetos User no banco de dados.
   @Query("SELECT e FROM User e JOIN FETCH e.roles WHERE e.username= (:username)")
   public User findByUsername(@Param("username") String username);
 
